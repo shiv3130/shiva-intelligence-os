87986249f30f3b75d1017860ff8b23a8688305a6
venkata(Shiva) gogireddy <shivareddy3130@gmail.com>
2026-06-04 23:41:11 +0530

    update

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'Jb7x-Veyh-99yhMb2RfrsKNUpo0_WWFC',
+            'X-App-Proxy': 'jAwDgWsmJAQyub51KqPUUGromB0WfvfK',
           },
           body: JSON.stringify(requestDetails),
         };
